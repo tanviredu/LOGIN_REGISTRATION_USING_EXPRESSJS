17f3115cdafaf74d977a202542147aea7e9a33e7
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2020-08-07 14:47:34 +0600

    added

--- routes/signuprouter.js
+++ routes/signuprouter.js
@@ -1,8 +1,11 @@
 const express      = require("express");
 const signuprouter = express.Router();
+const nunjucks = require("nunjucks");
 const user         = require("../models/user");
 
-
+signuprouter.get("/",(req,res)=>{
+    res.render("registration.html");
+})
 
 
 
